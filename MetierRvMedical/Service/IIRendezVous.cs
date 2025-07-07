b54e7f9d3427b79f238b685300c20828dbdd5d25
Amy-Diop <amyd8303@gmail.com>
2025-07-07 23:02:49 +0200

    Conflits corrigés

---
+++ MetierRvMedical/Service/IIRendezVous.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using MetierRvMedical.Model;
+
+namespace MetierRvMedical.Service
+{
+    [ServiceContract]
+    public interface IIRendezVous
+    {
+        [OperationContract]
+        RendezVous GetRendezVousById(int id);
+
+        [OperationContract]
+        bool AddRendezVous(RendezVous rv);
+
+        [OperationContract]
+        bool UpdateRendezVous(RendezVous rv);
+
+        [OperationContract]
+        void DeleteRendezVous(int id);
+
+        [OperationContract]
+        List<RendezVous> GetAllRendezVous();
+    }
+}
---
+++ MetierRvMedical/Service/IRendezVous.svc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using MetierRvMedical.Model;
+
+namespace MetierRvMedical.Service
+{
+    public class IRendezVous : IIRendezVous
+    {
+        private readonly BdRvMedicalContext db = new BdRvMedicalContext();
+
+        public RendezVous GetRendezVousById(int id)
+        {
+            return db.RendezVous.Find(id);
+        }
+
+        public bool AddRendezVous(RendezVous rv)
+        {
+            try
+            {
+                db.RendezVous.Add(rv);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateRendezVous(RendezVous rv)
+        {
+            try
+            {
+                db.Entry(rv).State = EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void DeleteRendezVous(int id)
+        {
+            var rv = db.RendezVous.Find(id);
+            if (rv != null)
+            {
+                db.RendezVous.Remove(rv);
+                db.SaveChanges();
+            }
+        }
+
+        public List<RendezVous> GetAllRendezVous()
+        {
+            return db.RendezVous.ToList();
+        }
+    }
+}
