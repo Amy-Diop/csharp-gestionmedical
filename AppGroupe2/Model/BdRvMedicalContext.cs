df7435e7d7a671e60809925a04b37bba262fb110
Amy-Diop <amyd8303@gmail.com>
2025-07-08 01:37:01 +0200

    Mise A Jour Renndez-Vous

--- AppGroupe2/Model/BdRvMedicalContext.cs
+++ AppGroupe2/Model/BdRvMedicalContext.cs
@@ -34,6 +34,14 @@ namespace AppGroupe2.Model
 
         public DbSet<Role> roles { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Supprimer tout schéma par défaut (évite dbo.)
+            modelBuilder.HasDefaultSchema("");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
 
 
 
